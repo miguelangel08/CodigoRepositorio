188b91beeb6c35a7ec3a0a0062130f093a4d82e1
miguelangel08 <miguel.angel0893@outlook.com>
2021-09-15 18:26:27 -0500

    Se agrega el proyecto

---
+++ funciones_matematicas.py
@@ -0,0 +1,5 @@
+def suma (valor1, valor2):
+    print("El resultado de la suma es: ", valor1 + valor2)
+
+def suma (valor1, valor2):
+    print("El resultado de la suma es: ", valor1 - valor2)
---
+++ funciones_min_max.py
@@ -0,0 +1,11 @@
+def minimo(valor1, valor2):
+    if(valor1 < valor2):
+        return valor1
+    else:
+        return valor2
+
+def maximo (valor1, valor2):
+    if(valor1 > valor2):
+        return valor1
+    else:
+        return valor2
